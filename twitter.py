d19cd92e23c44083099fe6a7f273d2ced7522f0c
mayank0508 <thakurmayank0508@gmail.com>
2021-02-14 02:14:15 +0530

    retweet function enabeled

--- twitter.py
+++ twitter.py
@@ -2,7 +2,6 @@ import tweepy
 import time
 
 auth = tweepy.OAuthHandler('H6gFo3kndfuxJJkxO9c38nN6X','jLbhFvYvp12WZ1Co9bysn52y8TkVKAujRaESIr7UnJKdGkaETd')
-
 auth.set_access_token('1360279899433410562-AOrLTpmqRprwcnEIoBwvU1MZwTlEVY', 'jtmOu9zeVWucUiB168ToGYmPeGBTR1FiQYGokJaxdytWG')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -10,13 +9,13 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user = api.me()
 
 search = 'bitcoin'
-nrTweets = 500
+nrTweets = 1000
 
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
        print('Tweet Retweeted')
        tweet.retweet()
-       time.sleep(5)
+       time.sleep(30)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
