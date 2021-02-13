538a440ba1dc9b4a01f38c53c4028cd90ce903d9
mayank0508 <thakurmayank0508@gmail.com>
2021-02-13 23:20:11 +0530

    Import package

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('','')
+auth = tweepy.OAuthHandler('ofVW1WX28hsxh1bveZaA5FhGh','Qj8RRTD4jDcjY5ELoACS3FQzMWtZ5i03nT6pQT6qluLUEoir37')
 
+auth.set_access_token('1360279899433410562-N4zoHGhWKQ5GZVH9maZ45Zuqja5jdO', 'szyC5Jtt76pyzgxR5USASwcuFDjKQZiXo2MJpckIKwWCR')
